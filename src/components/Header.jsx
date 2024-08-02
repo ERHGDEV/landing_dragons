313b413a12797385e6d5cc582062d84ee401a965
ERHDDEV <erhgdev@gmail.com>
2024-08-02 12:40:03 -0600

    fix del título

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,7 @@
 const Header = () => {
     return (
         <header className="header">
-        <h1>API Dragons in the Song of ICe and Fire</h1>
+        <h1>API Dragons of A Song of ICe and Fire</h1>
         </header>
     )
 }
