81eac546b163b74c2b9ca30f9cb5e0c7e5fb8979
ERHDDEV <erhgdev@gmail.com>
2024-08-05 18:26:14 -0600

    update padding del header y urls

--- src/services/dragons.js
+++ src/services/dragons.js
@@ -1,6 +1,6 @@
 import axios from "axios"
 
-const API_URL = "https://dragons-api-dun.vercel.app/api/dragons"
+const API_URL = "https://dragons-api-ten.vercel.app/api/dragons"
 
 const getAll = () => {
     const request = axios.get(API_URL)
